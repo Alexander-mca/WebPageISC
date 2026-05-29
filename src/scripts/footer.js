170478e2d48e4e31c58c95f67d9c719e746ccadd
Alex Morales <denisalexandermorales@gmail.com>
2026-05-28 18:46:17 -0600

    Commit 2
    Se agrego el footer a todas las paginas

---
+++ src/scripts/footer.js
@@ -0,0 +1,6 @@
+// Carga el navbar en todas las páginas
+      fetch('src/paginas/footer.html')
+        .then(response => response.text())
+        .then(html => {
+        document.getElementById('footer-container').innerHTML = html;
+        });
